cad8faad72ea6e94223d6605ecbefdd4ba7dab0d
Juli084 <juliorodrigues84@live.com>
2024-07-07 22:14:49 -0300

    iniciando banner component

--- src/components/header/index.tsx
+++ src/components/header/index.tsx
@@ -3,6 +3,22 @@ import { Ionicons, Feather } from "@expo/vector-icons";
 
 export function Header () {
     return (
-        <View />
+        <View className="w-full flex flex-row items-center justify-between">
+            <Pressable className="w-10 h-10 bg-slate-50 rounded-full flex justify-center items-center">
+                <Ionicons name="menu" size={25} color="#131313"/>
+            </Pressable>
+
+            <View className="flex flex-col items-center justify-center">
+                <Text className="text-center text-sm text-slate-900"> Localização</Text>
+                <View className="flex-row items-center justify-center gap-1">
+                    <Feather name="map-pin" size={15} color="#ff0000" />
+                    <Text className="text-lg font-bold">Sorocaba-SP</Text>
+                </View>
+            </View>
+
+            <Pressable className="w-10 h-10 bg-slate-50 rounded-full flex justify-center items-center">
+                <Feather name="bell" size={25} color="#131313"/>
+            </Pressable>
+        </View>
     );
 }
